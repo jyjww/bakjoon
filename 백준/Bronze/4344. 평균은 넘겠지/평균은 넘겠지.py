72480605f98362b55cb86b4e5020a5ce050c3ce1
YeJee Jang <158022863+jyjww@users.noreply.github.com>
2025-03-14 21:21:48 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,16 +1,8 @@
 a = int(input())
 
 for __ in range(a):
-    result = input().split()
-
-    total = 0
-    for i in range(1,len(result)):
-        total += int(result[i])
-
-    average = total / int(result[0])
-    count=0
-    for i in range(1, len(result)):
-        if average < int(result[i]):
-            count += 1
-    answer = count / int(result[0])
-    print(f'{answer*100:.3f}%')
+    result = list(map(int, input().split()))
+    
+    avg = sum(result[1:]) / result[0]
+    count = sum(1 for x in result[1:] if x > avg)
+    print(f"{count/sum(result[:1])*100:.3f}%")
