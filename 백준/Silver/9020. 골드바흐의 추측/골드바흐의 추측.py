e1ce5d2c0eb48f06661710757efcf6b5d33d7eb1
YeJee Jang <158022863+jyjww@users.noreply.github.com>
2025-03-15 14:03:06 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 344 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,21 +1,14 @@
+#에라토스테네스의 체
+prime = [True]*10001
+prime[0:2]=[False, False]
+for i in range (2, 10001):
+    for j in range (i+i, 10001, i):
+        prime[j]=False
 
-def is_prime(a):
-    if (a<2):
-        return False
-    for i in range(2,a):
-        if (a%i == 0):
-            return False
-    return True
 a = int(input())
-
-for i in range(a):
-    b= int(input())
-    half = b // 2
-    A, B = half, half
-    for j in range (half):
-        if is_prime(A) and is_prime(B):
-            print(A, B)
+for __ in range(a):
+    b=int(input())
+    for i in range(b//2, 10000):
+        if prime[b-i] and prime[i]:
+            print(b-i, i)
             break
-        else :
-            A -= 1
-            B += 1
