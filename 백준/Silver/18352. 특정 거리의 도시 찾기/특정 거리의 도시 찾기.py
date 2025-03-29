402b5dd8b83bee3e9afe96ea34a14bd616f74aaf
YeJee Jang <158022863+jyjww@users.noreply.github.com>
2025-03-30 02:27:50 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1528 ms, Memory: 116828 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -5,31 +5,31 @@ input = sys.stdin.readline
 def bfs(v):
     queue = deque()
     queue.append(v)
-    visited[v] = 1
-    distance[v] = 0
+    visited[v] = 0
     while queue:
         now = queue.popleft()
+        if visited[now] >= K:
+            continue
         for nextN in graph[now]:
-            if not visited[nextN]:
-                visited[nextN] = 1
-                distance[nextN] = distance[now] +1
+            if visited[nextN] == -1:
+                visited[nextN] = visited[now] + 1
                 queue.append(nextN)
 
 N, M, K, X = map(int, input().split())
 graph = defaultdict(list)
-visited = [0]*(N+1)
-distance = [0]*(N+1)
+visited = [-1] * (N + 1)
+
 for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
-    
+
 bfs(X)
 result = []
-for i in range(1, N+1):
-    if distance[i] == K:
+for i in range(1, N + 1):
+    if visited[i] == K:
         result.append(i)
-        
-if result :
+
+if result:
     for city in sorted(result):
         print(city)
 else:
