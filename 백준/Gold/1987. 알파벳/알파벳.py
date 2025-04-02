1f9e01c87e3e0eba95149bf8b500976e25adc4f3
YeJee Jang <158022863+jyjww@users.noreply.github.com>
2025-04-03 01:26:32 +0900

    [Gold IV] Title: 알파벳, Time: 1048 ms, Memory: 51864 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,26 +1,26 @@
 import sys
-sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
-def dfs(r, c):
-    global max_count
-    max_count = max(max_count, len(letter))
-    for dy, dx in directions :
-        ny, nx = dy+r, dx+c
-        if 0 <= ny < R and 0 <= nx < C:
-            next_char = map[ny][nx]
-            if next_char not in letter :
-                letter.add(next_char)
-                dfs(ny, nx)
-                letter.remove(next_char) 
+def bfs(r, c):
+    max_count = 1
+    letter.add((r, c, map[r][c]))
+    
+    while letter:
+        r, c, path = letter.pop()
+        for dy, dx in directions :
+            ny, nx = dy+r, dx+c
+            if 0 <= ny < R and 0 <= nx < C:
+                next_char = map[ny][nx]
+                if next_char not in path:
+                    letter.add((ny, nx, path+map[ny][nx]))
+                    max_count = max(max_count, len(path)+1)
     return max_count
-                        
+
 R, C = map(int, input().split())
 map = [list(map(str, input().strip())) for _ in range(R)]
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 max_count = 0
 letter = set()
-letter.add(map[0][0])
 
-ans = dfs(0, 0)
+ans = bfs(0, 0)
 print(ans)
